72496cc907b89eb442d227a79cba7f577be13fed
zumbojo <zumbojo@gmail.com>
2011-06-14 12:13:25 -0500

    Tiny bit of code; mostly just tests.  Finished 9.3.1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,12 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:session][:email],
                              params[:session][:password])
     if user.nil?
-      # Create an error message and re-render the signin form.
       flash.now[:error] = "Invalid email/password combination."
       @title = "Sign in"
       render 'new'
     else
-      # Sign the user in and redirect to the user's show page.
+      sign_in user
+      redirect_to user
     end
   end
 
