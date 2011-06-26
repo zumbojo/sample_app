b5ae1571d778c0590944d72755f0e1747d29c6ce
zumbojo <zumbojo@gmail.com>
2011-06-25 19:39:10 -0500

    "Done with user edit/update, index, and destroy actions".  User destruction is now available (only to admins).  Finished 10.4.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
 
   def show
     @user = User.find(params[:id])
@@ -51,6 +52,12 @@ class UsersController < ApplicationController
     @title = @user.name
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
   private
     
     def authenticate
@@ -60,5 +67,9 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
