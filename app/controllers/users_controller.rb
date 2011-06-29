2f5f57f395ad4de7c05fc9cba714fd47e1d2023f
zumbojo <zumbojo@gmail.com>
2011-06-28 21:19:38 -0500

    Microposts will now be displayed on the user's page.  In theory.  Finished 11.2.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,6 +57,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page => params[:page])
     @title = @user.name
   end
 
