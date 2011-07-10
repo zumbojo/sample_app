1459b2403b33b0d4889571f115a797539ecf76fb
zumbojo <zumbojo@gmail.com>
2011-07-10 17:11:24 -0500

    Micropost.from_users_followed_by fleshed out, fixing previous breakage. Finished 12.3.2

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,4 +7,8 @@ class Micropost < ActiveRecord::Base
   validates :user_id, :presence => true
 
   default_scope :order => 'microposts.created_at DESC'
+
+  def self.from_users_followed_by(user)
+    where(:user_id => user.following.push(user))
+  end
 end
